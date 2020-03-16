68838a56711b53e11404a2215c95729ba1aec8de
Arkady Pugachev <qweeex@yandex.com>
2020-03-16 22:39:54 +0400

    Start project

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import jQuery from "jquery";
+import popper from "popper.js";
+import bootstrap from "bootstrap";
+
+if (window.$ === undefined){
+  window.$ = jQuery;
+}
+
+jQuery(function() {
+
+});
